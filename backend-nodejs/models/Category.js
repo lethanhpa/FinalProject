0b9d593267a77e68a243b03c7e6a198592e6dbe5
nguyenthanhtung32 <binbungbu6201@gmail.com>
2024-01-22 18:06:58 +0700

    be

---
+++ backend-nodejs/models/Category.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const categorySchema = new Schema(
+  {
+    name: { type: String, required: [true, 'Category bắt buộc phải nhập'] },
+    description: { type: String, required: true },
+
+  },
+  {
+    versionKey: false,
+    timestamps: true
+
+  },
+);
+categorySchema.pre("create", function (next) {
+  next();
+});
+const Category = model('Category', categorySchema);
+
+module.exports = Category;
