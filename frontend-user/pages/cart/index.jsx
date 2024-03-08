95d1506540dcf471c0d7a1eff10bf3488227c8d8
lethanhpa <lethanhpa1406@gmail.com>
2024-03-08 16:41:19 +0700

    Pa push

--- frontend-user/pages/cart/index.jsx
+++ frontend-user/pages/cart/index.jsx
@@ -7,26 +7,33 @@ import {
   Trash2,
   Undo,
 } from "lucide-react";
+import {Result, Button} from "antd";
+import Link from "next/link";
 import axiosClient from "@/libraries/axiosClient";
 import numeral from "numeral";
-import jwt_decode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import { useRouter } from "next/router";
 
 function Carts() {
-  const router = useRouter;
+  const router = useRouter();
   const [carts, setCarts] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
     const calculateTotalPrice = () => {
       let total = 0;
-      carts.forEach((item) => {
-        item.products.forEach((product) => {
-          total +=
+
+      if (carts && Array.isArray(carts)) {
+        carts.forEach((item) => {
+          if (item.products && Array.isArray(item.products)) {
+            item.products.forEach((product) => {
+              total +=
             ((product.product.price * (100 - product.product.discount)) / 100) *
             product.quantity;
+            });
+          }
         });
-      });
+      }
       setTotalPrice(total);
     };
     calculateTotalPrice();
@@ -36,7 +43,7 @@ function Carts() {
     const fetchData = async () => {
       try {
         const token = localStorage.getItem("token");
-        const decoded = jwt_decode(token);
+        const decoded = jwtDecode(token);
         const customerId = decoded._id;
         console.log("««««« customerId »»»»»", customerId);
 
@@ -44,8 +51,8 @@ function Carts() {
           `http://localhost:9000/carts/${customerId}`
         );
         const data = response.data;
-        console.log("««««« data »»»»»", data);
         setCarts(data.payload.results);
+        console.log("««««« data »»»»»", data.payload.results);
       } catch (err) {
         console.error(err);
       }
@@ -58,13 +65,13 @@ function Carts() {
       <img src="https://file.hstatic.net/1000381168/file/baner-thanh-toan_78c520df795d4667b36605c554655bb1_master.png" />
       <div className="container mt-10">
         <table className="w-full mb-10">
-          <thead class="bg-black text-white font-roboto  space-x-4">
+          <thead className="bg-black text-white font-roboto  space-x-4">
             <tr>
-              <th class="border w-1/2 py-3">SẢN PHẨM</th>
-              <th class="border w-1/12 py-3">SỐ LƯỢNG</th>
-              <th class="border w-1/6 py-3">ĐƠN GIÁ</th>
-              <th class="border w-1/6 py-3">THÀNH TIỀN</th>
-              <th class="border w-1/12 py-3">
+              <th className="border w-1/2 py-3">SẢN PHẨM</th>
+              <th className="border w-1/12 py-3">SỐ LƯỢNG</th>
+              <th className="border w-1/6 py-3">ĐƠN GIÁ</th>
+              <th className="border w-1/6 py-3">THÀNH TIỀN</th>
+              <th className="border w-1/12 py-3">
                 <div className="flex justify-center items-center">
                   <AlertCircle />
                 </div>
@@ -72,25 +79,25 @@ function Carts() {
             </tr>
           </thead>
           <tbody className="border-b border-slate-400">
-            {carts.length > 0 &&
+            {carts && carts.length > 0 &&
               carts.map((item) =>
-                item.products.length > 0 ? (
-                  item.products.map((product) => (
+              item.cartDetails && item.cartDetails.length > 0 ? (
+                  item.cartDetails.map((product) => (
                     <tr key={product._id}>
-                      <td class="flex justify-center items-center">
-                        <div class="mr-4 my-5">
+                      <td className="flex justify-center items-center">
+                        <div className="mr-4 my-5">
                           <img
-                            class="w-32 bg-pink"
-                            src={product.product?.img}
+                            className="w-[200px] h-auto bg-pink"
+                            src={product.product?.imageUrl}
                             alt="Product Image"
                           />
                         </div>
                         <div>
-                          <p class="font-elle font-light">
+                          <p className="font-elle font-light">
                             {product.product?.name}
                           </p>
                           <p>
-                            <strong>Size:</strong>{" "}
+                            <strong>Size:</strong>
                             <span>{product.product?.size}</span>
                           </p>
                         </div>
@@ -136,6 +143,7 @@ function Carts() {
                   ))
                 ) : (
                   <Result
+                  key="no-products"
                     title="There are no products in your cart yet"
                     extra={
                       <Button
@@ -151,36 +159,36 @@ function Carts() {
               )}
           </tbody>
         </table>
-        <div class="my-10 flex justify-end">
-          <div class="my-3">
-            <div class="flex justify-end">
-              <div class="flex">
+        <div className="my-10 flex justify-end">
+          <div className="my-3">
+            <div className="flex justify-end">
+              <div className="flex">
                 <CircleDollarSign size={24} strokeWidth={1} />
-                <p class="ml-2 font-roboto font-bold text-xl">
+                <p className="ml-2 font-roboto font-bold text-xl">
                   TỔNG TIỀN (tạm tính)
                 </p>
               </div>
-              <div class="ml-10">
-                <span class="font-roboto font-bold text-lg">
+              <div className="ml-10">
+                <span className="font-roboto font-bold text-lg">
                   {numeral(totalPrice).format("0,0")}đ
                 </span>
               </div>
             </div>
-            <div class="my-3 flex justify-end">
-              <span class="font-roboto w-3/4">
+            <div className="my-3 flex justify-end">
+              <span className="font-roboto w-3/4">
                 Thời gian nhận hàng từ 7 - 15 ngày (trường hợp sớm hơn chúng tôi
                 sẽ thông báo trước cho Quý khách!)
               </span>
             </div>
-            <div class="my-3 flex justify-end">
-              <div class="mr-3">
-                <button class="flex border py-3 px-6 bg-gray hover:bg-white text-white hover:text-black font-elle">
-                  <Undo class="mr-3" size={24} strokeWidth={1} /> CHỌN THÊM SẢN
+            <div className="my-3 flex justify-end">
+              <div className="mr-3">
+                <button className="flex border py-3 px-6 bg-gray hover:bg-white text-white hover:text-black font-elle">
+                  <Undo className="mr-3" size={24} strokeWidth={1} /> CHỌN THÊM SẢN
                   PHẨM KHÁC
                 </button>
               </div>
-              <div class="ml-3">
-                <button class="flex border py-3 px-20 bg-black hover:bg-white text-white hover:text-black font-elle">
+              <div className="ml-3">
+                <button className="flex border py-3 px-20 bg-black hover:bg-white text-white hover:text-black font-elle">
                   THANH TOÁN
                 </button>
               </div>
