283954fb57c5fc1f150406905fcf1ab78d45773b
hoangvinh <phanthoangvinh28@gmail.com>
2024-03-29 12:09:43 +0700

    push order

--- frontend-user/pages/purchase-history/index.jsx
+++ frontend-user/pages/purchase-history/index.jsx
@@ -1,18 +1,43 @@
 import React, { memo, useEffect, useState } from "react";
 import numeral from "numeral";
-import { BackTop, Modal, Input } from "antd";
+import { BackTop, Modal, Input, Rate, message } from "antd";
 import axiosClient from "@/libraries/axiosClient";
 import { API_URL } from "@/constants";
 import Moment from "moment";
 import { jwtDecode } from "jwt-decode";
+import { useRouter } from "next/router";
+
 const { TextArea } = Input;
 
 function PurchaseHistory() {
+  const router = useRouter();
   const [orders, setOrders] = useState([]);
   const [selectedProductId, setSelectedProductId] = useState(null);
+  const [reviewedProducts, setReviewedProducts] = useState({});
 
   useEffect(() => {
     fetchOrders();
+  }, []);
+
+  useEffect(() => {
+    if (orders.length > 0) {
+      const updatedReviewedProducts = orders.reduce((acc, order) => {
+        order.orderDetails.forEach(detail => {
+          if (reviewedProducts[detail.productId._id]) {
+            acc[detail.productId._id] = true;
+          }
+        });
+        return acc;
+      }, {});
+      setReviewedProducts(updatedReviewedProducts);
+    }
+  }, [orders]);
+
+  useEffect(() => {
+    const reviewedProductsFromStorage = JSON.parse(localStorage.getItem("reviewedProducts"));
+    if (reviewedProductsFromStorage) {
+      setReviewedProducts(reviewedProductsFromStorage);
+    }
   }, []);
 
   const fetchOrders = async () => {
@@ -33,41 +58,61 @@ function PurchaseHistory() {
   };
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
+
+  const handleRatingChange = (value) => {
+    setRating(value);
+  };
+
+  const handleCommentChange = (e) => {
+    setComment(e.target.value);
+  };
 
   const showModal = (productId) => {
     setSelectedProductId(productId);
     setIsModalOpen(true);
   };
 
-  const handleOk = () => {
-    setIsModalOpen(false);
+  const handleOk = async () => {
+    try {
+      if (rating === 0) {
+        message.error("Vui lòng chọn số sao để đánh giá sản phẩm.");
+        return;
+      }
+
+      const token = localStorage.getItem("token");
+      const decoded = jwtDecode(token);
+      const customerId = decoded._id;
+
+      const reviewData = {
+        customerId: customerId,
+        productId: selectedProductId._id,
+        ratingRate: rating,
+        comment: comment,
+        reviewDate: new Date()
+      };
+
+      const response = await axiosClient.post('/reviews', reviewData);
+
+      message.success("Bạn đã đánh giá thành công!");
+
+      const updatedReviewedProducts = {
+        ...reviewedProducts,
+        [selectedProductId._id]: true,
+      };
+      setReviewedProducts(updatedReviewedProducts);
+      localStorage.setItem("reviewedProducts", JSON.stringify(updatedReviewedProducts));
+
+      setIsModalOpen(false);
+    } catch (error) {
+      console.error('Error:', error);
+      message.error("Đã có lỗi xảy ra. Vui lòng thử lại sau.");
+    }
   };
 
   const handleCancel = () => {
     setIsModalOpen(false);
-  };
-
-  const getOrderAction = (status, orderDetails, orderId) => {
-    if (status === "COMPLETE") {
-      return (
-        <button
-          className="bg-primry text-white font-bold w-[150px] h-[40px] rounded-full hover:bg-white hover:text-primry hover:border-primry hover:border mr-5"
-          onClick={() => showModal(orderDetails.productId)}
-        >
-          Đánh giá
-        </button>
-      );
-    } else if (status === "WAITING") {
-      return (
-        <button
-          className="mr-5 bg-primry text-white font-bold w-[150px] h-[40px] rounded-full hover:bg-white hover:text-primry hover:border-primry hover:border"
-          onClick={() => handleCancelOrder(orderId)}
-        >
-          Hủy đơn hàng
-        </button>
-      );
-    }
-    return null;
   };
 
   const handleCancelOrder = async (orderId) => {
@@ -87,13 +132,55 @@ function PurchaseHistory() {
     }
   };
 
+  const getOrderAction = (status, productId, order) => {
+    console.log('order', order);
+    const isReviewed = reviewedProducts[productId._id];
+
+    if (status === "COMPLETE") {
+      if (!isReviewed) { // Nếu sản phẩm chưa được đánh giá
+        return (
+          <button
+            className="bg-primry text-white font-bold w-[150px] h-[40px] rounded-full hover:bg-white hover:text-primry hover:border-primry hover:border mr-5"
+            onClick={() => showModal(productId)}
+          >
+            Đánh giá
+          </button>
+        );
+      } else {
+        return (
+          <button
+            className="bg-primry text-white font-bold w-[150px] h-[40px] rounded-full hover:bg-white hover:text-primry hover:border-primry hover:border mr-5"
+            onClick={() => HandleMuaLai(productId)}
+          >
+            Mua lại
+          </button>
+        );
+      }
+    } else if (status === "WAITING") {
+      return (
+        <button
+          className="mr-5 bg-primry text-white font-bold w-[150px] h-[40px] rounded-full hover:bg-white hover:text-primry hover:border-primry hover:border"
+          onClick={() => handleCancelOrder(order)}
+        >
+          Hủy đơn hàng
+        </button>
+      );
+    }
+    return null;
+  };
+
+  const HandleMuaLai = (productId) => {
+    router.push(`/${productId._id}`);
+  }
+
   const getStatusText = (status) => {
     if (status === "COMPLETE") {
       return "Đã mua";
     } else if (status === "WAITING") {
       return "Đang đợi duyệt";
-    } else status === "CANCELED";
-    return "Đã hủy";
+    } else {
+      return "Đã hủy";
+    }
   };
 
   return (
@@ -103,86 +190,88 @@ function PurchaseHistory() {
           LỊCH SỬ MUA HÀNG
         </h1>
       </div>
-      <div className="flex flex-col mt-[20px]">
+      <div className="flex flex-col mt-[20px]" style={{
+        background: "-webkit-linear-gradient(top,#fff 0%,#f7f7f7 100%)",
+      }}>
         {orders &&
           orders
             .filter((order) => order.status !== "CANCELED")
             .map((order) => (
               <div key={order._id}>
-                {order.orderDetails.map((detail, index) => (
-                  <div key={index} className="flex shadow-xl border-b">
-                    <div className="max-w-[250px]">
-                      <img
-                        src={`${API_URL}${detail.imageUrl}`}
-                        alt={`Image-${detail._id}`}
-                        className="object-contain"
-                      />
-                    </div>
-                    <div className="flex items-center gap-[30px]">
-                      <div className="w-full flex flex-col justify-center items-center md:items-start gap-[16px] ">
-                        <h3 className="font-bold font-roboto w-[300px]">
-                          {detail.productName}
-                        </h3>
-                        <div className="flex justify-start items-start flex-col gap-[8px] max-w-[400px]">
-                          {/* <p className="text-sm">
-                        <span className="font-roboto">Mã : </span> {detail.ma}
-                      </p>
-                      <p className="text-sm  ">
-                        <span className="font-roboto">Size : </span>
-                        {detail.size}
-                      </p> */}
-                          <p className="text-sm font-roboto">
-                            <span>Số lượng: </span>
-                            {detail.quantity}
+                {order.orderDetails.map((detail, index) => {
+                  const totalAmount = detail.price * detail.quantity;
+                  return (
+                    <div key={index} className="flex shadow-xl border-b">
+                      <div className="max-w-[250px]">
+                        <img
+                          src={`${API_URL}${detail.imageUrl}`}
+                          alt={`Image-${detail._id}`}
+                          className="object-contain"
+                        />
+                      </div>
+                      <div className="flex items-center gap-[30px]">
+                        <div className="w-full flex flex-col justify-center items-center md:items-start gap-[16px] ">
+                          <h3 className="font-bold font-roboto w-[300px]">
+                            {detail.productName}
+                          </h3>
+                          <div className="flex justify-start items-start flex-col gap-[8px] max-w-[400px]">
+                            <p className="text-sm font-roboto">
+                              <span>Số lượng: </span>
+                              {detail.quantity}
+                            </p>
+                            <p className="text-sm font-roboto">
+                              <span>Giá: </span>
+                              {numeral(totalAmount).format("0,0")}đ
+                            </p>
+                          </div>
+                        </div>
+                        <p className="text-lg flex justify-center items-center font-roboto font-medium w-full">
+                          {getStatusText(order.status)}
+                        </p>
+                        <div className="flex xl:gap-[180px] lg:gap-[100px]">
+                          <p className="text-lg flex justify-center items-center font-roboto ">
+                            {Moment(`${order.createdDate}`).format("DD/MM/YYYY")}
                           </p>
-                          <p className="text-sm font-roboto">
-                            <span>Giá: </span>
-                            {numeral(detail.price).format("0,0")}đ
-                          </p>
+                          {getOrderAction(order.status, detail.productId, order._id)}
+                          <Modal
+                            title={"Đánh giá sản phẩm"}
+                            visible={isModalOpen}
+                            onOk={handleOk}
+                            onCancel={handleCancel}
+                            className="font-roboto text-sm"
+                          >
+                            {selectedProductId && orders && orders.map(order => (
+                              order.orderDetails.map((detail, index) => {
+                                if (detail.productId === selectedProductId) {
+                                  return (
+                                    <div key={index}>
+                                      <div className="flex">
+                                        <div className="max-w-[100px]">
+                                          <img
+                                            src={`${API_URL}${detail.imageUrl}`}
+                                            alt={`Image-${detail._id}`}
+                                            className="object-contain"
+                                          />
+                                        </div>
+                                        <h3 className="font-roboto flex items-center justify-center">
+                                          {detail.productName}
+                                        </h3>
+                                      </div>
+                                      <div className="flex gap-8">
+                                        <p className="font-roboto mb-4">Chất lượng sản phẩm</p>
+                                        <Rate allowHalf defaultValue={0} onChange={handleRatingChange} />
+                                      </div>
+                                      <TextArea placeholder="Hãy chia sẻ những điều bạn thích về sản phẩm này với người mua khác nhé!" allowClear value={comment} onChange={handleCommentChange} />
+                                    </div>
+                                  );
+                                }
+                              })
+                            ))}
+                          </Modal>
                         </div>
                       </div>
-                      <p className="text-lg flex justify-center items-center font-roboto font-medium w-full">
-                        {getStatusText(order.status)}
-                      </p>
-                      <div className="flex xl:gap-[180px] lg:gap-[100px]">
-                        <p className="text-lg flex justify-center items-center font-roboto ">
-                          {Moment(`${order.createdDate}`).format("DD/MM/YYYY")}
-                        </p>
-                        {getOrderAction(order.status, detail, order._id)}
-                        <Modal
-                          title="Đánh giá sản phẩm"
-                          visible={isModalOpen}
-                          onOk={handleOk}
-                          onCancel={handleCancel}
-                          className="font-roboto text-sm"
-                        >
-                          <div className="flex">
-                            <div className="max-w-[100px]">
-                              <img
-                                src={`${API_URL}${detail.imageUrl}`}
-                                alt={`Image-${detail._id}`}
-                                className="object-contain"
-                              />
-                            </div>
-                            <h3 className="font-roboto flex items-center justify-center">
-                              {detail.productName}
-                            </h3>
-                          </div>
-                          <div className="flex gap-8">
-                            <p className="font-roboto mb-4">
-                              Chất lượng sản phẩm
-                            </p>
-                            <p>hiện sao</p>
-                          </div>
-                          <TextArea
-                            placeholder="Hãy chia sẽ những điều bạn thích về sản phẩm này với người mua khác nhé!"
-                            allowClear
-                          />
-                        </Modal>
-                      </div>
-                    </div>
-                  </div>
-                ))}
+                    </div>);
+                })}
               </div>
             ))}
         {orders &&
@@ -269,6 +358,7 @@ function PurchaseHistory() {
               </div>
             ))}
       </div>
+
       <BackTop />
     </div>
   );
