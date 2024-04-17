65039ce170d6bbf5d1adc60479cc824ef64ecbea
nguyenthanhtung32 <binbungbu6201@gmail.com>
2024-04-17 16:48:26 +0700

    fix bug vnpay

--- frontend-user/pages/payment/index.jsx
+++ frontend-user/pages/payment/index.jsx
@@ -34,7 +34,7 @@ function Payment() {
           console.log(err);
         }
       } else if (vnp_TransactionStatus) {
-        return;
+        localStorage.removeItem("orderInfo");
       }
     };
     handleAddOrder();
@@ -61,73 +61,77 @@ function Payment() {
             : "Giao dịch của bạn không thành công. Vui lòng thử lại sau."}
         </p>
       </div>
-      <div>
-        <p className="ml-36 mt-5 font-bold text-sm">Nội dung đơn hàng:</p>
-        <div className="justify-center items-center flex">
-          <table className="mt-5 w-2/3">
-            <thead className="font-roboto space-x-4">
-              <tr>
-                <th className="border border-solid border-gray-500 w-1/2 py-3">
-                  Tên sản phẩm
-                </th>
-                <th className="border border-solid border-gray-500 w-1/12 py-3">
-                  Ngày đặt
-                </th>
-                <th className="border border-solid border-gray-500 w-1/6 py-3">
-                  Đơn giá
-                </th>
-                <th className="border border-solid border-gray-500 w-1/6 py-3">
-                  Số lượng
-                </th>
-                <th className="border border-solid border-gray-500 w-1/6 py-3">
-                  Tổng
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {orderInfo &&
-                orderInfo.orderDetails &&
-                orderInfo.orderDetails.length > 0 && (
-                  <tr className="border border-solid border-gray-500">
-                    <td className="flex items-center justify-center py-3">
-                      <p className="font-elle font-light">
-                        {orderInfo.orderDetails[0].productName}
-                      </p>
-                    </td>
-                    <td className="border border-solid border-gray-500">
-                      <div className="flex items-center justify-center py-3">
-                        {moment(orderInfo.createdDate).format("DD/MM/YYYY")}
-                      </div>
-                    </td>
-                    <td className="border border-solid border-gray-500">
-                      <div className="flex justify-center items-center py-3">
-                        {numeral(orderInfo.orderDetails[0].price).format("0,0")}
-                        đ
-                      </div>
-                    </td>
-                    <td className="border border-solid border-gray-500">
-                      <div className="flex justify-center items-center py-3">
-                        {orderInfo.orderDetails[0].quantity}
-                      </div>
-                    </td>
-                    <td className="border border-solid border-gray-500">
-                      <div className="flex items-center justify-center py-3">
-                        {numeral(
-                          orderInfo.orderDetails[0].price *
-                            orderInfo.orderDetails[0].quantity
-                        ).format("0,0")}
-                        đ
-                      </div>
-                    </td>
-                  </tr>
-                )}
-            </tbody>
-          </table>
+      {isSuccess && (
+        <div>
+          <p className="ml-36 mt-5 font-bold text-sm">Nội dung đơn hàng:</p>
+          <div className="justify-center items-center flex">
+            <table className="mt-5 w-2/3">
+              <thead className="font-roboto space-x-4">
+                <tr>
+                  <th className="border border-solid border-gray-500 w-1/2 py-3">
+                    Tên sản phẩm
+                  </th>
+                  <th className="border border-solid border-gray-500 w-1/12 py-3">
+                    Ngày đặt
+                  </th>
+                  <th className="border border-solid border-gray-500 w-1/6 py-3">
+                    Đơn giá
+                  </th>
+                  <th className="border border-solid border-gray-500 w-1/6 py-3">
+                    Số lượng
+                  </th>
+                  <th className="border border-solid border-gray-500 w-1/6 py-3">
+                    Tổng
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {orderInfo &&
+                  orderInfo.orderDetails &&
+                  orderInfo.orderDetails.length > 0 && (
+                    <tr className="border border-solid border-gray-500">
+                      <td className="flex items-center justify-center py-3">
+                        <p className="font-elle font-light">
+                          {orderInfo.orderDetails[0].productName}
+                        </p>
+                      </td>
+                      <td className="border border-solid border-gray-500">
+                        <div className="flex items-center justify-center py-3">
+                          {moment(orderInfo.createdDate).format("DD/MM/YYYY")}
+                        </div>
+                      </td>
+                      <td className="border border-solid border-gray-500">
+                        <div className="flex justify-center items-center py-3">
+                          {numeral(orderInfo.orderDetails[0].price).format(
+                            "0,0"
+                          )}
+                          đ
+                        </div>
+                      </td>
+                      <td className="border border-solid border-gray-500">
+                        <div className="flex justify-center items-center py-3">
+                          {orderInfo.orderDetails[0].quantity}
+                        </div>
+                      </td>
+                      <td className="border border-solid border-gray-500">
+                        <div className="flex items-center justify-center py-3">
+                          {numeral(
+                            orderInfo.orderDetails[0].price *
+                              orderInfo.orderDetails[0].quantity
+                          ).format("0,0")}
+                          đ
+                        </div>
+                      </td>
+                    </tr>
+                  )}
+              </tbody>
+            </table>
+          </div>
+          <p className="ml-36 text-xs mt-1 text-stone-500">
+            Lưu ý: Bạn có thể xem chi tiết đơn hàng trong lịch sử mua hàng
+          </p>
         </div>
-        <p className="ml-36 text-xs mt-1 text-stone-500">
-          Lưu ý: Bạn có thể xem chi tiết đơn hàng trong lịch sử mua hàng
-        </p>
-      </div>
+      )}
       <div className="flex justify-around items-center mx-40 my-10">
         <Link href="/products">
           <button className="border rounded-md py-1 px-1 bg-black text-white hover:bg-white hover:text-black transition duration-300 ease-in-out transform hover:-translate-y-1 active:translate-y-0">
